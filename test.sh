acafe0c14b4c6ea23098e3df99c32068b90d1670
Hidetatsu Yaginuma <deetyler@protonmail.com>
2021-03-25 17:38:34 +0900

    add test script to make sure no race condition

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# make sure race condition is detected
+output=$(go test -run Test_SingleThreadQueue_Push_Pop -race ./...)
+if ! grep -q "WARNING: DATA RACE" <<< $output; then
+    echo race condition is not detected in SingleThreadQueue test
+    exit 1
+fi
+
+# make sure race condition is NOT detected
+output=$(go test -run Test_MutexQueue_Push_Pop -race ./...)
+if grep -q "WARNING: DATA RACE" <<< $output; then
+    echo race condition is detected in MutexQueue test
+    exit 1
+fi
+
+# make sure race condition is NOT detected
+output=$(go test -run Test_SizeLimitedQueue_Push_Pop -race ./...)
+if grep -q "WARNING: DATA RACE" <<< $output; then
+    echo race condition is detected in MutexQueue test
+    exit 1
+fi
